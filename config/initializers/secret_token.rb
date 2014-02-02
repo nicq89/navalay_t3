f4ca1ffb1a589636bad2a1b19ee6d6dff7a59c08
ubuntu <ubuntu@ubuntuVirtualBox.(none)>
2014-02-03 00:29:03 +0100

    Create new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NavalayT3::Application.config.secret_key_base = '766862885e63715b1c03cb7ed5f4900ae687fdef53e576b8ba8ef9ec7f71324bcc9ec71a70dc6268494d82bc5a2a47701de605a538deb761450e04cc8ea00d51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NavalayT3::Application.config.session_store :cookie_store, key: '_navalay_t3_session'
